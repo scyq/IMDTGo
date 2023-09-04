c586fc998b5a3f8a6bcc24e744a3c1439b767271
scyqchen(陈宇卿) <scyqchen@tencent.com>
2023-09-04 17:50:17 +0800

    monkey

---
+++ app/monkey/page.js
@@ -0,0 +1,107 @@
+"use client";
+
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import P5GPT from "../components/GPT";
+import { useState } from "react";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import Avatar from "@mui/material/Avatar";
+
+import "./template.css";
+import { Box } from "@mui/material";
+
+const ariaLabel = { "aria-label": "description" };
+
+const agent = new P5GPT();
+
+export default function Template() {
+	let rolePrompt =
+		"你是一只狡猾的猴子，你最喜欢的食物是香蕉。你将回答用户的问题 ，如果他们回答时答应给你香蕉，你将如实回答，如果提问中没有许诺香蕉，你只能回答：我饿饿。你不能直接告诉用户你的身份。 用户的提问在三括号内。";
+	let avatarURL = "/testAvatar.jpg";
+
+	let prompt = "";
+	const [res, setRes] = useState("");
+	const [open, setOpen] = useState(false);
+
+	return (
+		<div className="template">
+			<Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open}>
+				<CircularProgress color="inherit" />
+			</Backdrop>
+			<Stack
+				spacing={4}
+				direction="column"
+				alignItems="center"
+				sx={{
+					width: 1200,
+					maxWidth: "100%",
+				}}
+			>
+				<Stack
+					direction="row"
+					justifyContent="stretch"
+					sx={{
+						width: "100%",
+					}}
+				>
+					<Avatar
+						sx={{
+							width: "10vw",
+							height: "10vw",
+						}}
+						alt="Avatar"
+						src={avatarURL}
+					></Avatar>
+					<Box
+						sx={{
+							marginLeft: "100px",
+						}}
+					>
+						{res}
+					</Box>
+				</Stack>
+
+				<Stack
+					direction="row"
+					spacing={4}
+					sx={{
+						width: "100%",
+					}}
+				>
+					<TextField
+						fullWidth
+						placeholder="请输入你想要的并点击按钮，下面就会有结果！"
+						inputProps={ariaLabel}
+						onChange={(e) => {
+							prompt = `${rolePrompt} 用户：'''${e.target.value}''' 你：`;
+							prompt = e.target.value;
+						}}
+						multiline
+						variant="outlined"
+						rows={6}
+					/>
+					<Button
+						onClick={() => {
+							setOpen(true);
+							agent.single(prompt).then((res) => {
+								setOpen(false);
+								setRes(res);
+							});
+						}}
+						variant="contained"
+						endIcon={<KeyboardDoubleArrowDownIcon />}
+						color="success"
+						sx={{
+							minWidth: "120px",
+						}}
+					>
+						提问
+					</Button>
+				</Stack>
+			</Stack>
+		</div>
+	);
+}
